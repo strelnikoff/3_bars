5846e8dd4692d874d6a7e6a97a0cc16d43d4eb3b
Oleg <oleglip48@gmail.com>
2016-10-09 21:00:05 +0300

    pep8 правки

--- bars.py
+++ bars.py
@@ -1,4 +1,6 @@
-import json, os, argparse
+import json
+import os
+import argparse
 
 
 def load_data(filepath):
@@ -10,17 +12,17 @@ def load_data(filepath):
 
 def get_biggest_bar(args):
     data = load_data(args.file)
-    return max(data, key=lambda x:x["Cells"].get("SeatsCount"))
+    return max(data, key=lambda x: x["Cells"].get("SeatsCount"))
 
 
 def get_smallest_bar(args):
     data = load_data(args.file)
-    return min(data, key=lambda x:x["Cells"].get("SeatsCount"))
+    return min(data, key=lambda x: x["Cells"].get("SeatsCount"))
 
 
 def get_closest_bar(args):
     data = load_data(args.file)
-    return min(data, key=lambda x:calculate_distance(get_point(x), [args.longitude, args.latitude]))
+    return min(data, key=lambda x: calculate_distance(get_point(x), [args.longitude, args.latitude]))
 
 
 def calculate_distance(point_one, point_two):
@@ -35,18 +37,18 @@ def get_point(bar):
 def parse_args():
     parser = argparse.ArgumentParser(description='Bar json utility')
     subparsers = parser.add_subparsers()
-    parser.add_argument('file', type = str, help = 'Json file with bars')
+    parser.add_argument('file', type=str, help='Json file with bars')
 
-    parser_biggest = subparsers.add_parser('biggest', help = 'Rerurn biggest bar')
-    parser_biggest.set_defaults(func = get_biggest_bar)
+    parser_biggest = subparsers.add_parser('biggest', help='Rerurn biggest bar')
+    parser_biggest.set_defaults(func=get_biggest_bar)
 
-    parser_smallest = subparsers.add_parser('smallest', help = 'Rerurn smallest bar')
-    parser_smallest.set_defaults(func = get_smallest_bar) 
+    parser_smallest = subparsers.add_parser('smallest', help='Rerurn smallest bar')
+    parser_smallest.set_defaults(func=get_smallest_bar)
 
-    parser_closest = subparsers.add_parser('closest', help = 'Rerurn closest bar')
-    parser_closest.add_argument('longitude', type = float, help = 'you longitude')
-    parser_closest.add_argument('latitude', type = float, help = 'you latitude')
-    parser_closest.set_defaults(func = get_closest_bar)
+    parser_closest = subparsers.add_parser('closest', help='Rerurn closest bar')
+    parser_closest.add_argument('longitude', type=float, help='you longitude')
+    parser_closest.add_argument('latitude', type=float, help='you latitude')
+    parser_closest.set_defaults(func=get_closest_bar)
 
     return parser.parse_args()
 
